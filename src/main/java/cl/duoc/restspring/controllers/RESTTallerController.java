891d6dd0f186b37e7218dd6e88d90e293e45e4ba
FakeWing <carlos.moralesu@usach.cl>
2019-08-27 10:27:05 -0400

    testing
    testing

---
+++ src/main/java/cl/duoc/restspring/controllers/RESTTallerController.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.duoc.restspring.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RESTTallerController {
+    
+}
